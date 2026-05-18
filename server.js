77c87d1e1438b5df397ae4d396a75aa8539365de
Bruno Lima <bruuno.lima03@gmail.com>
2026-05-18 10:20:09 -0300

    refactor: correct architecture

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 app.get("/", (request, response) => {
   request.send("Servidor funcionando");
 });
 app.listen(3000, (request, response) => {
   console.log("Servidor na porta 3000");
 });
-app.use(express.json());
 const router = require("./routes/users");
 app.use("/usuarios", router);
