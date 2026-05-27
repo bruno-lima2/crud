e3b453c720f8c21b2c51811f8f7a98af2d9b8301
Bruno Lima <bruuno.lima03@gmail.com>
2026-05-27 11:08:35 -0300

    refactor: review

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -2,7 +2,7 @@ const { z } = require("zod");
 const createUserSchema = z.object({
   name: z
     .string({ required_error: "Nome é obrigatório" })
-    .min(3, "Nome deve ter pelo menos 3 caracteres")
-    .max(100, "Nome muito grande"),
+    .min(3, "O nome deve ter pelo menos 3 caracteres")
+    .max(100, "O nome é muito grande"),
 });
 module.exports = { createUserSchema };
