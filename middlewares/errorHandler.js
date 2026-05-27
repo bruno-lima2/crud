bd976b3705d8da0ac619de2baf2e4a8fa32c1f21
Bruno Lima <bruuno.lima03@gmail.com>
2026-05-27 09:18:39 -0300

    refactor: add zod to error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,12 @@
+const { ZodError } = require("zod");
 function errorHandler(error, request, response, next) {
   console.error(error);
+  if (error instanceof ZodError) {
+    return response.status(400).json({
+      success: false,
+      message: error.issues[0].message,
+    });
+  }
   response.status(error.statusCode || 500).json({
     success: false,
     message: error.message || "Erro interno no servidor",
