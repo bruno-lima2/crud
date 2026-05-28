c6bbf73f8235e0c71b05eff2d2d6d1764f127558
Bruno Lima <bruuno.lima03@gmail.com>
2026-05-28 10:37:44 -0300

    refactor: review

--- schemas/idSchema.js
+++ schemas/idSchema.js
@@ -1,8 +1,8 @@
 const { z } = require("zod");
 const createIdSchema = z.object({
   id: z.coerce
-    .number({ invalid_type_error: "o ID deve ser um número" })
-    .int("o ID deve ser inteiro")
-    .positive("o ID é inválido"),
+    .number({ invalid_type_error: "ID deve ser um número" })
+    .int("ID deve ser um número inteiro")
+    .positive("ID inválido"),
 });
 module.exports = { createIdSchema };
